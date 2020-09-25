611a85852f2943dfb03b1d233e9bdbf832228e14
annaadhiambo <annaadhiambo01@gmail.com>
2020-09-25 09:55:09 +0300

    first commit

---
+++ app/main/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, RadioField,SelectField
+from wtforms.validators import Required
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    # vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')
+
+class PitchForm(FlaskForm):
+    category_id = SelectField('Select Category', choices=[('1', 'Toppings'), ('2', 'Flavours'), ('3', 'Crust')])
+    content = TextAreaField('make a request', validators=[Required()])
+    submit = SubmitField('Order Pizza') 
+
+class UpvoteForm(FlaskForm):
+    '''
+    Class to create a wtf form for upvoting a pitch
+    '''
+    submit = SubmitField('Upvote')
+
+class DownvoteForm(FlaskForm):
+    '''
+    Class to create a wtf form for downvoting a pitch
+    '''
+    submit = SubmitField('Downvote')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
